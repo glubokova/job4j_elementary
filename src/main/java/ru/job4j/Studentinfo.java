6dd74bd65895c4084ac42c19de0a9415bb3d8236
HP <glubokova_aida@mail.ru>
2023-12-28 01:49:45 +0500

    3.3 Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println ("Aida Glubokova") ;
+        System.out.println ("09.08.2001") ;
+
+
+}
+}
