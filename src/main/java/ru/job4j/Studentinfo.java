79cb829e0f84a4bd5e7a07de890cb6ba8da9e7e1
Аида <glubokova_aida@mail.ru>
2024-01-03 23:26:59 +0500

    4.1. Аргументы [#295521]

--- src/main/java/ru/job4j/Studentinfo.java
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -2,9 +2,7 @@ package ru.job4j;
 
 public class Studentinfo {
     public static void main(String[] args) {
-        System.out.println ("Aida Glubokova") ;
-        System.out.println ("09.08.2001") ;
-
-
-}
+        System.out.println("Aida Glubokova");
+        System.out.println("09.08.2001");
+    }
 }
