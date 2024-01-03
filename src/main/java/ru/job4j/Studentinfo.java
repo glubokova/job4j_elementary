dc05a48e15dbd629798d1dc418e50a806080124e
Аида <glubokova_aida@mail.ru>
2024-01-03 23:56:28 +0500

    3.3 Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/Studentinfo.java
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class Studentinfo {
     public static void main(String[] args) {
-        System.out.println("Aida Glubokova");
+        System.out.println("Aida Glubokovа");
         System.out.println("09.08.2001");
     }
 }
