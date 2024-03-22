627bba246778d5f39c52bd3473550e4a81747383
Аида <glubokova_aida@mail.ru>
2024-03-22 23:03:52 +0500

    5.6. Простые числа [#156314]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,23 +2,16 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        if (number <= 1) {
+        boolean prime = true;
+        if (number == 1) {
             return false;
         }
-        for (int i = 2; i * i <= number; i++) {
+        for (int i = 2; i < number - 1; i++) {
             if (number % i == 0) {
-                return false;
+                prime = false;
+                break;
             }
         }
-        return true;
-    }
-
-    public static void main(String[] args) {
-        int num = 17;
-        if (check(num)) {
-            System.out.println(num + " is a prime number. ");
-        } else {
-            System.out.println(num + " is not a prime number. ");
-        }
+        return prime;
     }
 }
