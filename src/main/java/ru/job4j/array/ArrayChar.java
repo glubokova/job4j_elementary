9dc9d866f09e9f017d48ca16cdcad7af41bc9e7e
Аида <glubokova_aida@mail.ru>
2024-04-11 17:28:38 +0500

    6.4.0. Слово начинается с ... [#41585]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -4,8 +4,6 @@ public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
         for (int i = 0; i < pref.length; i++) {
-            char p = pref[i];
-            char w = word[i];
             if (pref[i] != word[i]) {
                 result = false;
                 break;
