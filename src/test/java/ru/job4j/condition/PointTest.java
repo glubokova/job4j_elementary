0346a51265708deb5b037573b8ffe41ddee4650d
Аида <glubokova_aida@mail.ru>
2024-07-20 15:33:21 +0500

    2.0. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.withPrecision;
 
 class PointTest {
