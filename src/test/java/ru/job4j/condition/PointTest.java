629d9c312da813e84f1a32bebdc14afbaec274f1
Аида <glubokova_aida@mail.ru>
2024-07-26 15:40:52 +0500

    2.0. Рефакторинг - Расстояние между точками. [#122587 #505950]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -8,20 +8,6 @@ class PointTest {
 
     @Test
     void when00to20then2() {
-        class Point {
-            private int x;
-            private int y;
-
-            public Point(int x, int y) {
-                this.x = x;
-                this.y = y;
-            }
-
-            public double distance(Point that) {
-                return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
-            }
-        }
-
         Point a = new Point(0, 0);
         Point b = new Point(2, 0);
         double expected = 2.0;
@@ -31,20 +17,6 @@ class PointTest {
 
     @Test
     void when10To13Then3() {
-        class Point {
-            private int x;
-            private int y;
-
-            public Point(int x, int y) {
-                this.x = x;
-                this.y = y;
-            }
-
-            public double distance(Point that) {
-                return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
-            }
-        }
-
         Point a = new Point(1, 0);
         Point b = new Point(1, 3);
         double expected = 3.0;
@@ -54,24 +26,10 @@ class PointTest {
 
     @Test
     void when00to02then2() {
-        class Point {
-            private int x;
-            private int y;
-
-            public Point(int x, int y) {
-                this.x = x;
-                this.y = y;
-            }
-
-            public double distance(Point that) {
-                return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
-            }
-        }
-
         Point a = new Point(0, 0);
         Point b = new Point(0, 2);
         double expected = 2.0;
-        double out = a.distance(b );
+        double out = a.distance(b);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
 }
